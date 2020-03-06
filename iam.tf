6cfc011a0f84c533c7b4a2cbbad73c390caafcfb
heromahir <mhr2495@gmail.com>
2020-03-05 20:22:08 -0600

    user tf added

---
+++ iam.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_user" "tim" { 
+   name = "tim"
+   path = "/"
+}
+resource "aws_iam_group_membership" "developers_team" { 
+  name = "developers-group-membership" 
+  users = [
+    aws_iam_user.tim.name
+  ]
+  group = aws_iam_group.developers.name
+}
+ 
