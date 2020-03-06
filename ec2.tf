3cbc5c7d495f1ccc382f3f49c6976e84b8e6240e
heromahir <mhr2495@gmail.com>
2020-03-05 20:16:07 -0600

    as

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "web" {
-  count       = 5
+  count       = 3
   ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.deployer.key_name}"
+}
+
+resource "aws_instance" "web2" {
+  ami          = "ami-0a887e401f7654935"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.deployer.key_name}"
+  
+}
 
   tags = {
     Name = "HelloWorld"
