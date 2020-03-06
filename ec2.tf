2ca940cce5186062207f30fc84a8425f8e47a4ca
heromahir <mhr2495@gmail.com>
2020-03-05 19:58:33 -0600

    key name interpolation added

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   count       = 5
   ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.deployer.key_name}"
 
   tags = {
     Name = "HelloWorld"
